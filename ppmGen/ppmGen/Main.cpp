66e1070313a2ec3abcef5c6785115199c6270d0b
Demetri V.S <demetriv.s.7@gmail.com>
2017-01-20 10:47:46 -0800

    Started to addcode
    +++Added includes and constants

---
+++ ppmGen/ppmGen/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int COLOR_PAGE = 3;
+const int ROWS = 10;
+const int COLS = 7;
+
+const int RED = 0;
+const int GREEN = 1;
+const int BLUE = 2;
+
+void createImage()
